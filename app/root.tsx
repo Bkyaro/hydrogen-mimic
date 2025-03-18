74e1e716e179d48a9d49768a659245ee7577e3ff
Jackal_lzt <bkyarokun@gmail.com>
2025-03-18 18:09:16 +0800

    Updates root file boilerplate

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,49 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./styles/app.css";
+
+export const links: LinksFunction = () => {
+	return [
+		{ rel: "stylesheet", href: styles },
+		{
+			rel: "preconnect",
+			href: "https://cdn.shopify.com",
+		},
+		{
+			rel: "preconnect",
+			href: "https://shop.app",
+		},
+		{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+	];
+};
+
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "Hydrogen",
+	viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
